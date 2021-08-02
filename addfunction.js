40748366c510274aaa1947a0135d56b88367da2c
AlexHamn <agalexandro16@gmail.com>
2021-08-02 17:51:24 -0500

    add bg imgs and menu links

--- addfunction.js
+++ addfunction.js
@@ -5,24 +5,39 @@ let ul=document.createElement('ul')
 let li0=document.createElement('li')
 let li1=document.createElement('li')
 let li2=document.createElement('li')
-let li3=document.createElement('li')
+let li3 = document.createElement('li')
+let a0 = document.createElement('a');
+let a1 = document.createElement('a');
+let a2 = document.createElement('a');
+let a3 = document.createElement('a');
 
 function mobile_menu() {
     body.append(section);
     section.append(i0);
     section.append(ul);
-    ul.append(li0,li1,li2,li3)
+    ul.append(li0, li1, li2, li3)
+    li0.append(a0);
+    li1.append(a1);
+    li2.append(a2);
+    li3.append(a3);
 
     section.setAttribute('class', 'Menubutton')
     i0.setAttribute('class', 'material-icons')
     ul.setAttribute('class', 'menu_ul')
+    a0.setAttribute('href', '#hello');
+    a0.setAttribute('onclick','section.remove()')
+    a1.setAttribute('href', '#portfolio');
+    a1.setAttribute('onclick','section.remove()')
+    a2.setAttribute('href', '#about');
+    a2.setAttribute('onclick','section.remove()')
+    a3.setAttribute('href', '#contact');
+    a3.setAttribute('onclick','section.remove()')
 
     i0.textContent='close';
     i0.setAttribute('onclick','section.remove()')
 
-    li0.textContent='Hello';
-    li1.textContent='Portfolio';
-    li2.textContent='About';
-    li3.textContent='Contact';
+    a0.textContent='Hello';
+    a1.textContent='Portfolio';
+    a2.textContent='About';
+    a3.textContent='Contact';
 }
-mobile_menu()
